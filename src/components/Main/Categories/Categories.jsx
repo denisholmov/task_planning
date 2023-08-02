62568ec486cc1d05c6a1badec88f74e4ba660027
Denis <example@gmail.com>
2023-08-02 16:50:21 +0300

    Сохраняю проект перед работой с fetch запросом

--- src/components/Main/Categories/Categories.jsx
+++ src/components/Main/Categories/Categories.jsx
@@ -4,6 +4,10 @@ import TaskList from "./TaskList/TaskList";
 import sectionTasks from "../../../assets/sectionTasks.json";
 
 const Categories = () => {
+  // https://64ca5c17700d50e3c704c7f0.mockapi.io/itemsTask - ссылка на mockApi
+
+  //   fetch("https://64ca5c17700d50e3c704c7f0.mockapi.io/itemsTask");
+
   return (
     <div className={classes.categories}>
       {sectionTasks.map((item) => (
